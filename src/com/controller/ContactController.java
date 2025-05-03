a970c43d1e5e91220e72db959338325762de5d41
msRashid276 <rashidmohammed276@gmail.com>
2025-05-03 19:50:27 +0530

    controller created , service creation started,

--- src/com/controller/ContactController.java
+++ src/com/controller/ContactController.java
@@ -2,20 +2,54 @@ package com.controller;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
+import java.net.URI;
+import java.util.List;
 
+import com.dto.ContactDto;
+import com.service.ContactService;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public class ContactController {
 	
+	
+	
 	public static void registerRoutes(HttpServer httpServer) {
-		httpServer.createContext("/contacts", new ContactHandler());
+		ContactService service = new ContactService();
+		httpServer.createContext("/contacts", new ContactHandler(service));
 	}
 	
 	static class ContactHandler implements HttpHandler {
+		
+		private final ContactService service;
+		
+		public ContactHandler(ContactService service) {
+			this.service = service ;
+		}
 
 		@Override
-		public void handle(HttpExchange exchange) throws IOException {			
+		public void handle(HttpExchange exchange) throws IOException {	
+			String method = exchange.getRequestMethod();
+			URI uri = exchange.getRequestURI();
+			String path = uri.getPath();
+			String[] pathParts = path.split("/");
+			int id = -1;
+			if(pathParts.length == 3) {
+				id = Integer.parseInt(pathParts[2]);
+			}
+			
+			switch(method) {
+				case "GET" :
+					List<ContactDto> contacts = service.getAllContacts();
+					respond(exchange,contacts);
+					break;
+			}
+		}
+		
+		
+		
+		private void respond(HttpExchange exchange, Object objectRes) {
+			
 		}
 		
 	}
--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,7 +1,13 @@
 package com.main;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.controller.ContactController;
+import com.sun.net.httpserver.HttpServer;
 
 import com.util.DBConnectionUtil;
 
@@ -9,9 +15,13 @@ public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		DBConnectionUtil.init();
-
+		HttpServer httpServer = HttpServer.create(new InetSocketAddress(8080), 0) ;
+		ContactController.registerRoutes(httpServer);
+		httpServer.setExecutor(null);
+		httpServer.start();
+		logger.info("Server started at http://localhost:8080");
 	}
 
 }
