c11f9694cd3216f3aed6505d955a56f2e2303e6a
msRashid276 <rashidmohammed276@gmail.com>
2025-05-04 22:57:21 +0530

    gson jar added, respond method created

--- src/com/controller/ContactController.java
+++ src/com/controller/ContactController.java
@@ -2,29 +2,35 @@ package com.controller;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URI;
 import java.util.List;
 
+
 import com.dto.ContactDto;
+import com.google.gson.Gson;
 import com.service.ContactService;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public class ContactController {
-	
-	
+		
 	
 	public static void registerRoutes(HttpServer httpServer) {
 		ContactService service = new ContactService();
-		httpServer.createContext("/contacts", new ContactHandler(service));
+		Gson gson = new Gson();
+
+		httpServer.createContext("/contacts", new ContactHandler(service,gson));
 	}
 	
 	static class ContactHandler implements HttpHandler {
 		
 		private final ContactService service;
+		private final Gson gson;
 		
-		public ContactHandler(ContactService service) {
+		public ContactHandler(ContactService service,Gson gson) {
 			this.service = service ;
+			this.gson = gson ;
 		}
 
 		@Override
@@ -48,8 +54,13 @@ public class ContactController {
 		
 		
 		
-		private void respond(HttpExchange exchange, Object objectRes) {
-			
+		private void respond(HttpExchange exchange, Object objectRes) throws IOException {
+			String response = gson.toJson(objectRes);
+			exchange.getResponseHeaders().set("Content-Type", "application/json");
+			exchange.sendResponseHeaders(200, response.getBytes().length);
+			OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
 		}
 		
 	}
